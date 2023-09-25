878379df29101bc407f5c44c66e7a1bc7c2eac9d
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-09-25 09:51:18 +0300

    STUNDU SARAKSTS...

--- src/FirstDiena.js
+++ src/FirstDiena.js
@@ -1,6 +1,31 @@
 function FirstDiena() {
     return (
-      <div class="box">Pirmdiena</div>
+      <table>
+<thead>
+  <tr>
+    <th>pirmdiena</th>
+    <th>25 septembris</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>1. Sistemu programesana </td>
+    <td>c.203 J.Krigerts</td>
+  </tr>
+  <tr>
+    <td>2. Dabaszinibas</td>
+    <td>c.207 M.Strade</td>
+  </tr>
+  <tr>
+    <td>3. Vesture</td>
+    <td>c.206 I.Eglite</td>
+  </tr>
+  <tr>
+    <td>4. Krievu valoda</td>
+    <td>c.201 D.Tarasova</td>
+  </tr>
+</tbody>
+</table>
     );
 }
 
